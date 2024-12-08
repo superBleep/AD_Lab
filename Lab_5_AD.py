53170eaeb4d91e93ae484d7df453002fa303a55b
nubaz <lucapaulf@gmail.com>
2024-12-08 21:48:16 +0200

    Lab 5 v3

--- Lab_5_AD.py
+++ Lab_5_AD.py
@@ -6,6 +6,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as ptch
+from scipy.io import loadmat
+from pyod.utils.utility import standardizer
+from pyod.models.pca import PCA
+from pyod.models.kpca import KPCA
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+from keras import Model, Sequential, layers
 
 
 # Exercise 1
@@ -130,5 +138,149 @@ def ex1():
     plt.show()
 
 
+# Exercise 2
+def ex2():
+    # Step 1
+    data = loadmat('./shuttle.mat')
+    X, Y = np.array(data['X']), np.array(data['y'])
+    N = np.shape(X)[0]
+
+    X = standardizer(X) # Standardization
+
+    # Compute real contamination rate
+    outliers = np.size(Y[np.where(Y == 1)])
+    total = np.size(Y)
+    cont_rate = np.round(outliers / total, 2)
+
+    test_size = int(0.6 * N) # 60% of data
+    train_size = N - test_size
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, train_size=train_size)
+
+    pca = PCA(contamination=cont_rate)
+    pca.fit(X_train)
+    
+    ind_exp_var = pca.explained_variance_
+    cum_exp_var = np.cumsum(ind_exp_var) / np.sum(ind_exp_var)
+
+    fig = plt.figure(1, figsize=(10, 5))
+
+    ax2 = fig.add_subplot(1, 2, 1)
+    ax2.step(range(1, len(cum_exp_var) + 1), cum_exp_var)
+    ax2.set_xticks(range(1, len(cum_exp_var) + 1))
+    ax2.set_xlabel('Component')
+    ax2.set_ylabel('Variance')
+    ax2.set_title('Cummulative explained variances')
+
+    ax3 = fig.add_subplot(1, 2, 2)
+    ax3.bar(range(1, len(ind_exp_var) + 1), ind_exp_var,color='red')
+    ax3.set_xticks(range(1, len(cum_exp_var) + 1))
+    ax3.set_xlabel('Component')
+    ax3.set_ylabel('Variance')
+    ax3.set_title('Individual variances')
+
+    plt.show()
+
+    # Step 2
+    Y_train_pred, Y_test_pred = pca.predict(X_train), pca.predict(X_test)
+
+    train_ba = balanced_accuracy_score(Y_train, Y_train_pred)
+    test_ba = balanced_accuracy_score(Y_test, Y_test_pred)
+
+    print("### BA (Train / Test sets, PCA) ###")
+    print("Train: ", train_ba)
+    print("Test: ", test_ba)
+
+    kpca = KPCA(cont_rate, kernel='linear')
+    kpca.fit(X_train)
+
+    Y_train_pred, Y_test_pred = kpca.predict(X_train), kpca.predict(X_test)
+
+    train_ba = balanced_accuracy_score(Y_train, Y_test_pred)
+    test_ba = balanced_accuracy_score(Y_test, Y_test_pred)
+
+    print("### BA (Train / Test sets, KPCA) ###")
+    print("Train: ", train_ba)
+    print("Test: ", test_ba)
+
+
+# Step 2
+class Autoencoder(Model):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+
+        self.encoder = Sequential([
+            layers.Dense(8, 'relu'),
+            layers.Dense(5, 'relu'),
+            layers.Dense(3, 'relu')
+        ])
+
+        self.decoder = Sequential([
+            layers.Dense(5, 'relu'),
+            layers.Dense(8, 'relu'),
+            layers.Dense(9, 'sigmoid')
+        ])
+
+    def call(self, inputs):
+        encode = self.encoder(inputs)
+        decode = self.decoder(encode)
+
+        return decode
+
+
+# Exercise 2
+def ex3():
+    # Step 1
+    data = loadmat('./shuttle.mat')
+    X, Y = np.array(data['X']), np.array(data['y'])
+    N = np.shape(X)[0]
+
+    X = MinMaxScaler((0, 1)).fit_transform(X) # Scale data to [0,1]
+
+    test_size = int(0.6 * N) # 50% of data
+    train_size = N - test_size
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, train_size=train_size)
+
+    # Step 3
+    autoencoder = Autoencoder()
+    autoencoder.compile('adam', 'mse')
+    history = autoencoder.fit(X_train, X_train, batch_size=1024, epochs=100, validation_data=(X_test, Y_test))
+
+    plt.figure(1)
+
+    plt.plot(history.history['loss'], label='Training loss')
+    plt.plot(history.history['val_loss'], label='Validation loss')
+
+    plt.xlabel('Epochs')
+    plt.xlabel('Loss')
+    plt.title('Training and validation losses')
+    plt.legend()
+
+    plt.show()
+
+    # Step 4
+    X_train_rec, X_test_rec = autoencoder.predict(X_train), autoencoder.predict(X_test)
+    
+    # Compute MSE
+    train_rec_err = np.mean((X_train - X_train_rec) ** 2, axis=1)
+    test_rec_err = np.mean((X_test - X_test_rec) ** 2, axis=1)
+
+    # Compute contamination rate
+    outliers = np.size(Y[np.where(Y == 1)])
+    total = np.size(Y)
+    cont_rate = np.round(outliers / total, 2)
+
+    train_thresh = np.quantile(train_rec_err, cont_rate)
+    test_thresh = np.quantile(test_rec_err, cont_rate)
+
+    Y_train_pred = np.array(train_rec_err < train_thresh, dtype=int)
+    Y_test_pred = np.array(test_rec_err < test_thresh, dtype=int)
+
+    print("### BA (Train / Test set) ###")
+    print("Train: ", balanced_accuracy_score(Y_train, Y_train_pred))
+    print("Test: ", balanced_accuracy_score(Y_test, Y_test_pred))
+
+
 if __name__ == '__main__':
     ex1()
+    ex2()
+    ex3()
