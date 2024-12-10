412e57e21bead813f4b36c17968fa5fdef4de52e
nubaz <lucapaulf@gmail.com>
2024-12-10 20:15:44 +0200

    Lab 5 v4

--- Lab_5_AD.py
+++ Lab_5_AD.py
@@ -13,12 +13,13 @@ from pyod.models.kpca import KPCA
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import balanced_accuracy_score
 from sklearn.preprocessing import MinMaxScaler
-from keras import Model, Sequential, layers
+from keras import Model, Sequential, layers, datasets
+import tensorflow as tf
 
 
-# Exercise 1
+# --- Exercise 1 ---
 def ex1():
-    # Step 1
+    # --- Step 1 ---
     mean = [5, 10, 2]
     cov = [
         [3, 2, 2],
@@ -44,7 +45,7 @@ def ex1():
     Sigma = np.cov(X_c, rowvar=False) # Compute covariance matrix
     eigenvalues, eigenvectors = np.linalg.eigh(Sigma) # EVD 
 
-    # Step 2
+    # --- Step 2 ---
     eigenvalues_desc = -np.sort(-eigenvalues)
     cum_exp_var = np.cumsum(eigenvalues_desc) / np.sum(eigenvalues_desc)
     ind_exp_var = eigenvalues_desc / np.sum(eigenvalues_desc)
@@ -65,8 +66,8 @@ def ex1():
 
     plt.show()
 
-    # Step 3
-    X_proj = np.dot(X_c, eigenvectors[:, 2]) # Projection along 3rd component
+    # --- Step 3 ---
+    X_proj = np.dot(X_c, eigenvectors[2, :]) # Projection along 3rd component
 
     cont_rate = 0.1
     thresh = np.quantile(X_proj, cont_rate)
@@ -87,7 +88,7 @@ def ex1():
     ax1.set_title('PCA Anomaly Detection (3d principal component)')
     ax1.legend(handles=[ptch.Patch(color="green", label="Inlier"), ptch.Patch(color="red", label="Outlier")])
 
-    X_proj = np.dot(X_c, eigenvectors[:, 1]) # Projection along 2nd component
+    X_proj = np.dot(X_c, eigenvectors[1, :]) # Projection along 2nd component
 
     cont_rate = 0.1
     thresh = np.quantile(X_proj, cont_rate)
@@ -109,7 +110,7 @@ def ex1():
 
     plt.show()
 
-    # Step 4
+    # --- Step 4 ---
     X_proj = np.dot(X_c, eigenvectors) # Project data along all components
     X_proj = np.dot(X_proj, np.sqrt(np.diag(eigenvalues))) # Normalize data
 
@@ -138,9 +139,9 @@ def ex1():
     plt.show()
 
 
-# Exercise 2
+# --- Exercise 2 ---
 def ex2():
-    # Step 1
+    # --- Step 1 ---
     data = loadmat('./shuttle.mat')
     X, Y = np.array(data['X']), np.array(data['y'])
     N = np.shape(X)[0]
@@ -180,7 +181,7 @@ def ex2():
 
     plt.show()
 
-    # Step 2
+    # --- Step 2 ---
     Y_train_pred, Y_test_pred = pca.predict(X_train), pca.predict(X_test)
 
     train_ba = balanced_accuracy_score(Y_train, Y_train_pred)
@@ -203,10 +204,10 @@ def ex2():
     print("Test: ", test_ba)
 
 
-# Step 2
+# --- Step 2 (Exercise 3) ---
 class Autoencoder(Model):
     def __init__(self):
-        super(Autoencoder, self).__init__()
+        super().__init__()
 
         self.encoder = Sequential([
             layers.Dense(8, 'relu'),
@@ -227,9 +228,9 @@ class Autoencoder(Model):
         return decode
 
 
-# Exercise 2
+# --- Exercise 3 ---
 def ex3():
-    # Step 1
+    # --- Step 1 ---
     data = loadmat('./shuttle.mat')
     X, Y = np.array(data['X']), np.array(data['y'])
     N = np.shape(X)[0]
@@ -240,10 +241,10 @@ def ex3():
     train_size = N - test_size
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, train_size=train_size)
 
-    # Step 3
+    # --- Step 3 ---
     autoencoder = Autoencoder()
     autoencoder.compile('adam', 'mse')
-    history = autoencoder.fit(X_train, X_train, batch_size=1024, epochs=100, validation_data=(X_test, Y_test))
+    history = autoencoder.fit(X_train, X_train, batch_size=1024, epochs=100, validation_data=(X_test, X_test))
 
     plt.figure(1)
 
@@ -257,7 +258,7 @@ def ex3():
 
     plt.show()
 
-    # Step 4
+    # --- Step 4 ---
     X_train_rec, X_test_rec = autoencoder.predict(X_train), autoencoder.predict(X_test)
     
     # Compute MSE
@@ -269,18 +270,110 @@ def ex3():
     total = np.size(Y)
     cont_rate = np.round(outliers / total, 2)
 
-    train_thresh = np.quantile(train_rec_err, cont_rate)
-    test_thresh = np.quantile(test_rec_err, cont_rate)
+    thresh = np.quantile(train_rec_err, cont_rate)
 
-    Y_train_pred = np.array(train_rec_err < train_thresh, dtype=int)
-    Y_test_pred = np.array(test_rec_err < test_thresh, dtype=int)
+    Y_train_pred = np.array(train_rec_err < thresh, dtype=int)
+    Y_test_pred = np.array(test_rec_err < thresh, dtype=int)
 
     print("### BA (Train / Test set) ###")
     print("Train: ", balanced_accuracy_score(Y_train, Y_train_pred))
     print("Test: ", balanced_accuracy_score(Y_test, Y_test_pred))
 
 
+# --- Step 2 (Exerice 4) ---
+class ConvAutoencoder(Model):
+    def __init__(self):
+        super().__init__()
+
+        self.encoder = Sequential([
+            layers.Conv2D(8, (3, 3), 2, 'same', activation='relu', input_shape=((28, 28, 1))),
+            layers.Conv2D(4, (3, 3), 2, 'same', activation='relu')
+        ])
+
+        self.decoder = Sequential([
+            layers.Conv2DTranspose(4, (3, 3), 2, 'same', activation='relu'),
+            layers.Conv2DTranspose(8, (3, 3), 2, 'same', activation='relu'),
+            layers.Conv2D(1, (3, 3), padding='same', activation='sigmoid')
+        ])
+
+    def call(self, inputs):
+        encode = self.encoder(inputs)
+        decode = self.decoder(encode)
+
+        return decode
+
+
+# --- Exercise 4 ---
+def ex4():
+    # --- Step 1 ---
+    (X_train, _), (X_test, _) = datasets.mnist.load_data()
+
+    # Normalize data
+    X_train = X_train / 255
+    X_test = X_test / 255
+
+    test_noise = tf.random.normal(np.shape(X_test)) # Generate noise
+    X_test_noise = tf.clip_by_value(X_test + test_noise, 0, 1) # Add noise to data
+
+    # --- Step 3 ---
+    conv_autoencoder = ConvAutoencoder()
+    conv_autoencoder.compile('adam', 'mse')
+    conv_autoencoder.fit(X_train, X_train, 64, 10, validation_data=(X_test, X_test))
+
+    X_train_rec = conv_autoencoder.predict(X_train)
+    X_test_rec = conv_autoencoder.predict(X_test)
+    X_test_noise_rec = conv_autoencoder.predict(X_test_noise)
+
+    # Reshape data back into original state
+    X_train_rec = np.reshape(X_train_rec, np.shape(X_train))
+    X_test_rec = np.reshape(X_test_rec, np.shape(X_test))
+    X_test_noise_rec = np.reshape(X_test_noise_rec, np.shape(X_test_noise))
+
+    # Compute reconstruction loss
+    train_rec_err = np.mean((X_train - X_train_rec) ** 2, axis=(1, 2))
+    test_rec_err = np.mean((X_test - X_test_rec) ** 2, axis=(1, 2))
+    test_noise_rec_err = np.mean((X_test_noise - X_test_noise_rec) ** 2, axis=(1, 2))
+
+    thresh = np.mean(train_rec_err) + np.std(train_rec_err)
+
+    Y_test_pred = np.array(test_rec_err > thresh, dtype=int)
+    Y_test_noise_pred = np.array(test_noise_rec_err > thresh, dtype=int)
+
+    Y_test = np.zeros(len(X_test), dtype=int) # "Clean" test set
+    Y_test_noise = np.ones(len(X_test_noise), dtype=int) # Anomalous test set 
+
+    print("### BA (Test / Test + noise sets) ###")
+    print("Test: ", balanced_accuracy_score(Y_test, Y_test_pred))
+    print("Test + noise: ", balanced_accuracy_score(Y_test_noise, Y_test_noise_pred))
+
+    # --- Step 4 --- 
+    _, axs = plt.subplots(4, 5)
+
+    # Original test images
+    for i in range(5):
+        axs[0, i].imshow(X_test[i], cmap='gray')
+        axs[0, i].axis('off')
+
+    # Test images with noise
+    for i in range(5):
+        axs[1, i].imshow(X_test_noise[i], cmap='gray')
+        axs[1, i].axis('off')
+
+    # Recunstructed test images
+    for i in range(5):
+        axs[2, i].imshow(X_test_rec[i], cmap='gray')
+        axs[2, i].axis('off')
+
+    # Recunstructed test images with noise
+    for i in range(5):
+        axs[3, i].imshow(X_test_noise_rec[i], cmap='gray')
+        axs[3, i].axis('off')
+
+    plt.show()
+
+
 if __name__ == '__main__':
     ex1()
     ex2()
     ex3()
+    ex4()
